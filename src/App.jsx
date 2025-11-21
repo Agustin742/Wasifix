fcafcdf67b32f96827c75a1f722e617cd3242a18
Agustin Tabarcache <agustintabarcache74@gmail.com>
2025-11-21 17:21:47 -0300

    Creacion del NavBar Responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import { BrowserRouter } from "react-router"
+import NavBar from "./components/layout/navbar/NavBar"
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <NavBar />
+      <div  className="h-1000 bg-[url('/imgs/mockups/manos.jpg')]"></div>
+
+
+    </BrowserRouter>
   )
 }
 
