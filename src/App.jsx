c2f8feb66fdcbea0e5ceda27a9c7d6232dec8013
Agustin Tabarcache <agustintabarcache74@gmail.com>
2025-11-22 00:35:17 -0300

    Style: Agregue el componente Card para usar de cardBase

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { BrowserRouter } from "react-router"
-import NavBar from "./components/layout/navbar/NavBar"
+import NavBar from "./components/layout/NavBar"
+import Card from "./components/ui/Card"
 
 
 const App = () => {
   return (
     <BrowserRouter>
       <NavBar />
-      <div  className="h-1000 bg-[url('/imgs/mockups/manos.jpg')]"></div>
-
+      <div  className="h-1000 bg-[url('/imgs/mockups/manos.jpg')] p-8 space-y-4">
+        <Card>Contenido Basico</Card>
+        <Card padding="lg" shadow="lg">Card grande</Card>
+        <Card hoverable clickable>Card Intercactiva</Card>
+      </div>
 
     </BrowserRouter>
   )
